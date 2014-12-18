2093fccc710d43b9633b05820aabfa61e6d3ae61
Joakim Stai <joakimstai@gmail.com>
2014-12-18 21:19:43 +0100

    Mark required configuration settings as such

--- config.js
+++ config.js
@@ -8,18 +8,18 @@ config.interval = process.env.SLACKUS_INTERVAL;
 // The number of recent comments to check (optional, default 25, max 100).
 config.disqus.limit = process.env.SLACKUS_DISQUS_LIMIT;
 
-// Disqus forum to check, specified by its shortname.
+// Disqus forum to check, specified by its shortname (required).
 // To check multiple forums, enter their shortnames separated by comma.
 config.disqus.forums = process.env.SLACKUS_DISQUS_FORUM;
 
-// Disqus authentication.
+// Disqus authentication (required).
 config.disqus.authentication = {
   api_secret: process.env.SLACKUS_DISQUS_API_SECRET,
   api_key: process.env.SLACKUS_DISQUS_API_KEY,
   access_token: process.env.SLACKUS_DISQUS_ACCESS_TOKEN,
 };
 
-// URL of your Slack Incoming WebHook.
+// URL of your Slack Incoming WebHook (required).
 // See https://my.slack.com/services.
 config.slack.webhook = process.env.SLACKUS_SLACK_WEBHOOK;
 
